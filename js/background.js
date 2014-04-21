f34d6d6c51bbb815374d415d5e62cca68a9eec1b
Mike Rogers <mike.r@fullondesign.co.uk>
2014-04-21 20:48:58 +0100

    adding in check for when domains have different domains to their support page

--- js/background.js
+++ js/background.js
@@ -3,15 +3,46 @@ var sites = {},
 // The odd subdomains people append to their site.
 subdomains = ['www', 'support', 'mail', 'ssl', 'new', 'cgi1', 'en', 'myaccount', 'meta', 'help', 'support', 'edit'];
 
-function getHostname(url, strict){
+
+function updateSites(){
+	var req = new XMLHttpRequest();
+	req.addEventListener("load", function(e){
+		if(this.status == "200"){
+			sites = JSON.parse(this.response);
+
+			// Update our local cache.
+			localStorage['sites'] = JSON.stringify(sites);
+			localStorage['lastUpdated'] = (new Date() * 1);
+		}
+	}, false);
+	req.open("GET", "https://raw.github.com/rmlewisuk/justdelete.me/dev/sites.json", true);
+	req.send();
+}
+
+function runUpdater(){
+  // Load the list of supported sites
+  if(typeof localStorage['sites'] != 'undefined'){
+    sites = JSON.parse(localStorage['sites']);
+
+    // If we haven't updated in the last day.
+    if(typeof localStorage['lastUpdated'] == "undefined" || localStorage['lastUpdated'] < new Date().setDate(new Date().getDate()-1)){
+      updateSites();
+    }
+  }else{
+    // Reload it via AJAX
+    updateSites();
+  }
+}
+
+// Turns url string into just the hostname.
+function getHostname(url, opts_strict){
 	var a = document.createElement('a');
 	a.href = url;
 
-	// If we need to do this quickly.
-	if(strict != true){
-		return a.hostname;
-	}
-	
+  if(opts_strict == true){
+    return a.hostname;
+  }
+
 	// Quickly strip any odd subdomains off
 	for(var i in subdomains){
 		url = url.replace('/'+subdomains[i]+'.', '/');
@@ -21,74 +52,43 @@ function getHostname(url, strict){
 	return a.hostname;
 }
 
-function updateSites(){
-	var req = new XMLHttpRequest();
-	req.addEventListener("load", function(e){
-		if(this.status == "200"){
-			var response = JSON.parse(this.response),
-				newSites = {};
-
-			// set up the new sites object.
-			for(var i in response){
-				// Vine is annoying and uses twitter.com for support.
-				if(response[i].name == 'Vine'){
-					newSites['vine.co'] = response[i];
-				// jsfiddle.net is also a pain and use github.
-				} else if(response[i].name == 'jsFiddle'){
-					newSites['jsfiddle.net'] = response[i];	
-				}else{
-					newSites[getHostname(response[i].url, true)] = response[i];	
-				}
-			}
-
-			sites = newSites;
-
-			// Update our local cache.
-			localStorage['sites'] = JSON.stringify(sites);
-			localStorage['lastUpdated'] = (new Date() * 1);
-		}
-	}, false);
-	req.open("GET", "https://raw.github.com/rmlewisuk/justdelete.me/master/sites.json", true);
-	req.send();
-}
-
-// Load the list of supported sites
-if(typeof localStorage['sites'] != 'undefined'){
-	sites = JSON.parse(localStorage['sites']);
-
-	// If we haven't updated in the last day.
-	if(typeof localStorage['lastUpdated'] == "undefined" || localStorage['lastUpdated'] < new Date().setDate(new Date().getDate()-1)){
-		updateSites();
-	}
-}else{
-	// Set up a temp sites array
-	sites['twitter.com'] = {
-		"name" : "Twitter",
-		"url" : "https://twitter.com/settings/accounts/confirm_deactivation",
-		"difficulty" : "easy"
-	};
-
-
-	// Update the local storage.
-	localStorage['sites'] = JSON.stringify(sites);
-
-	// Reload it via AJAX
-	updateSites();
-}
-
 /**
  * Cycle through the known sites and see if we have dirt on them.
  */
 function getInfo(url){
-	var hostname = getHostname(url, false);
+  // Start with a strict search
+	var hostname = getHostname(url, true);
+
 	for(var i in sites){
-		if(hostname.indexOf(i) != -1){
-			return sites[i]
-		}
+    var site = sites[i];
+
+    for(var d in site.domains){
+      var domain = site.domains[d];
+      if(domain.indexOf(hostname) != -1){
+        console.log(site, hostname);
+        return site;
+      }
+    }
+	}
+
+  // do a less strict search
+	hostname = getHostname(url, false);
+	for(var i in sites){
+    var site = sites[i];
+
+    for(var d in site.domains){
+      var domain = site.domains[d];
+      if(domain.indexOf(hostname) != -1){
+        console.log(site, hostname);
+        return site;
+      }
+    }
 	}
 
 	return false;
 }
+
+runUpdater();
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	if(changeInfo.status == 'loading'){
